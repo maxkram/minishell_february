64717be572461f9e8ab1448cad69e2d5c422c6de
maxkramer <max.kramer@live.com>
2024-02-26 13:21:05 -0500

    resolve some confilcts in makefile and main

--- src/main.c
+++ src/main.c
@@ -6,11 +6,11 @@
 /*   By: hezhukov <hezhukov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/22 18:28:49 by hezhukov          #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2024/02/25 15:53:05 by hezhukov         ###   ########.fr       */
-=======
+<<<<<<<<< Temporary merge branch 1
 /*   Updated: 2024/02/24 18:44:25 by device           ###   ########.fr       */
->>>>>>> be1f50c6f44b2f9656ea6389261df095f474c854
+=========
+/*   Updated: 2024/02/25 15:53:05 by hezhukov         ###   ########.fr       */
+>>>>>>>>> Temporary merge branch 2
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,8 @@ char	**path_getter(t_data *pnt, int i)
 
 	while (pnt->env[++i])
 	{
-		found = ft_strstr(pnt->env[i], "PATH=/home");
+		// found = ft_strstr(pnt->env[i], "PATH=/home");
+		found = ft_strstr(pnt->env[i], "PATH=");
 		if (found != NULL)
 			break ;
 	}
